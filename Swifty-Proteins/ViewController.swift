636f504195bd4a41070413d6af948be35ab92920
VKoshelev <VKoshelev@detmir.ru>
2022-10-23 14:50:02 +0300

    Initial Commit

---
+++ Swifty-Proteins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty-Proteins
+//
+//  Created by Vladislav Koshelev on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
