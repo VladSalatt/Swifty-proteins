7b15df46a173c4625b3fff71332295eca867f2c9
VKoshelev <VKoshelev@detmir.ru>
2022-11-04 17:39:24 +0300

    add swiftgen

--- Podfile
+++ Podfile
@@ -9,12 +9,5 @@ target 'Swifty-Proteins' do
 	pod 'RxDataSources'
 	pod 'SnapKit'
 	pod 'AppRouter'
-end
-
-post_install do |installer|
-    installer.pods_project.targets.each do |target|
-        target.build_configurations.each do |config|
-            config.build_settings['GCC_WARN_INHIBIT_ALL_WARNINGS'] = "YES"
-        end
-    end
+	pod 'SwiftGen'
 end
--- Swifty-Proteins/Scenes/Login/LoginView.swift
+++ Swifty-Proteins/Scenes/Login/LoginView.swift
@@ -1,18 +1,21 @@
 import RxCocoa
 import RxSwift
+import SnapKit
 import UIKit
 
 final class LoginView: UIView {
     private lazy var button: UIButton = {
-       let button = UIButton()
+        let button = UIButton()
+        button.setTitle("login", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
     }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .white
-            
-        
+        addSubview(button)
         makeConstraints()
         setupBindings()
     }
@@ -26,7 +29,11 @@ final class LoginView: UIView {
 extension Reactive where Base: LoginView {}
 
 private extension LoginView {
-    func makeConstraints() {}
+    func makeConstraints() {
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 
     func setupBindings() {}
 }
